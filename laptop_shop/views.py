44ddf3d1c4bd2c68b612dbc2bc5fbc453882e214
MuneebKhokhar786 <dev.muneeb.khokhar@gmail.com>
2023-11-30 13:13:01 -0800

    refactored views.py

--- laptop_shop/views.py
+++ laptop_shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 from django.urls import reverse_lazy
 from django.views import generic
@@ -10,57 +10,70 @@ from django.db.models import Count, OuterRef, Subquery
 from .models import Product, Collection, ProductImage
 from .decorators import ajax_login_required
 
+def get_first_product_image():
+    return ProductImage.objects.filter(
+        product=OuterRef('pk')).order_by('id')[:1]
+
+def get_user_id():
+    return cache.get('user_id')
+
+def get_user_cart(user_id):
+    return cache.get(user_id, {'cart': {}})['cart']
+
+def get_total_quantity(cart):
+    return sum(cart.values())
+
+def get_products_with_first_image():
+    products = Product.objects.all()
+    first_product_image = get_first_product_image()
+    return products.annotate(image=Subquery(first_product_image.values('image')))
+
+def get_cart_items(product_ids, cart_products):
+    cart_items = []
+    total_price = 0
+    total_quantity = 0
+    product_dict = {product.id: product for product in cart_products}
+
+    for product_id, quantity in product_ids.items():
+        product = product_dict.get(product_id)
+        if product:
+            cart_items.append({
+                'product': product,
+                'quantity': quantity,
+            })
+            total_price += product.price * quantity
+            total_quantity += quantity
+
+    return cart_items, total_price, total_quantity
 
 def home(request):
-    products = Product.objects.all()
-    first_product_image = ProductImage.objects.filter(
-        product=OuterRef('pk')).order_by('id')[:1]
-    products_with_first_image = products.annotate(
-        image=Subquery(first_product_image.values('image')))
+    products_with_first_image = get_products_with_first_image()
 
     collections = Collection.objects.annotate(product_count=Count(
         'product')).filter(product_count__gt=0).order_by('id')
-    for collection in collections:
-        collection_with_first_image = collection.product_set.annotate(
-            image=Subquery(first_product_image.values('image')))
-        collection.products = collection_with_first_image
 
-    user_id = cache.get('user_id')
-    cart = cache.get(user_id, {'cart': {}})['cart']
-    total_quantity = sum(cart.values())
+    for collection in collections:
+        collection.products = collection.product_set.annotate(
+            image=Subquery(get_first_product_image().values('image')))
+
+    cart = get_user_cart(get_user_id())
+    total_quantity = get_total_quantity(cart)
 
     return render(request, 'laptop_shop/home.html',
-                {'products': products_with_first_image, 'collections': collections, 'total_quantity': total_quantity})
-
+                  {'products': products_with_first_image, 'collections': collections, 'total_quantity': total_quantity})
 
 class SignupPageView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
 
-
 def update_cart(request):
-    user_id = cache.get('user_id')
-    cart = cache.get(user_id, {'cart': {}})['cart']
-
+    cart = get_user_cart(get_user_id())
     product_ids = list(cart.keys())
-    total_price = 0
-    total_quantity = 0
-    cart_items = []
 
-    if product_ids:
-        cart_products = Product.objects.filter(id__in=product_ids).prefetch_related('images')
-        product_dict = {product.id: product for product in cart_products}
+    cart_products = Product.objects.filter(id__in=product_ids)
 
-        for product_id, quantity in cart.items():
-            product = product_dict.get(product_id)
-            if product:
-                cart_items.append({
-                    'product': product,
-                    'quantity': quantity,
-                })
-                total_price += product.price * quantity
-                total_quantity += quantity
+    cart_items, total_price, total_quantity = get_cart_items(cart, cart_products)
 
     cart_html = render_to_string('laptop_shop/partial_cart_list.html',
                                 {'cart_items': cart_items, 'total_price': total_price, 'total_quantity': total_quantity})
@@ -72,22 +85,20 @@ def index(request, collection_name):
 
 @login_required
 def show(request, product_slug):
-    product = Product.objects.prefetch_related().get(slug__iexact=product_slug)
-    user_id = cache.get('user_id')
-    cart = cache.get(user_id, {'cart': {}})['cart']
-    total_quantity = sum(cart.values())
+    product = get_object_or_404(Product, slug__iexact=product_slug)
+    cart = get_user_cart(get_user_id())
+    total_quantity = get_total_quantity(cart)
     return render(request, 'laptop_shop/show.html', {'product': product, 'total_quantity': total_quantity})
 
 @ajax_login_required
 def add_to_cart(request, user_id, product_id, increment=1):
     cart = cache.get(user_id, {'cart': {}})['cart']
-    quantity = sum(cart.values())
+    quantity = get_total_quantity(cart)
 
-    if not cart.get(product_id, 0):
-        cart[product_id] = increment
-    else:
-        cart[product_id] += increment
+    if product_id not in cart:
+        cart[product_id] = 0
 
+    cart[product_id] += increment
     quantity += increment
 
     cache.set(user_id, {'cart': cart})
@@ -95,14 +106,14 @@ def add_to_cart(request, user_id, product_id, increment=1):
     return JsonResponse({'total_quantity': quantity})
 
 def remove_from_cart(request, product_id):
-    user_id = cache.get('user_id')
-    cart = cache.get(user_id, {'cart': {}})['cart']
-    total_quantity = sum(cart.values())
+    user_id = get_user_id()
+    cart = get_user_cart(user_id)
+    total_quantity = get_total_quantity(cart)
     quantity = 0
 
     if cart.get(product_id, 0):
         cart[product_id] -= 1
-        quantity = cart[product_id]
+        quantity = max(0, cart[product_id])
         if quantity == 0:
             del cart[product_id]
         total_quantity -= 1
