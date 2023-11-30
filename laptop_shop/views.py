5a7b76a3ce634d1591d87b41a6f62ae31ca844f5
MuneebKhokhar786 <dev.muneeb.khokhar@gmail.com>
2023-11-30 13:16:25 -0800

    minor change

--- laptop_shop/views.py
+++ laptop_shop/views.py
@@ -3,7 +3,6 @@ from django.http import JsonResponse
 from django.urls import reverse_lazy
 from django.views import generic
 from django.template.loader import render_to_string
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.core.cache import cache
 from django.db.models import Count, OuterRef, Subquery
