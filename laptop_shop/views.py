f5356793055363c2ff8bfa3a587b41d7cdf511be
MuneebKhokhar786 <dev.muneeb.khokhar@gmail.com>
2023-11-30 13:15:51 -0800

    minor change

--- laptop_shop/views.py
+++ laptop_shop/views.py
@@ -83,7 +83,6 @@ def index(request, collection_name):
     products = Product.objects.prefetch_related('images').filter(collection__name__iexact=collection_name)
     return render(request, 'laptop_shop/index.html', {'products': products})
 
-@login_required
 def show(request, product_slug):
     product = get_object_or_404(Product, slug__iexact=product_slug)
     cart = get_user_cart(get_user_id())
