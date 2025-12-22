ff3f2bede82e25b28cc5f2d8d694ffe52a74746b
M. Arhum Qayyum Khan <khanarhumqayyum@gmail.com>
2025-12-23 02:00:21 +0500

    feat(deletion) - Cloudinary Image Deletion
    Implemented Auto Image Deletion of media in cloudinary after the object is deleted to which the images are related to.

--- laptop_shop/signals.py
+++ laptop_shop/signals.py
@@ -1,6 +1,16 @@
 from django.contrib.auth.signals import user_logged_in
 from django.dispatch import receiver
+from django.db.models.signals import post_delete
+from .models import ProductImage
+
+import cloudinary.uploader
 
 @receiver(user_logged_in)
 def user_logged_in_handler(sender, request, user, **kwargs):
     request.session['user_id'] = user.id
+
+@receiver(post_delete, sender=ProductImage)
+def delete_cloudinary_image(sender, instance, **kwargs):
+    breakpoint()
+    if instance.image:
+        cloudinary.uploader.destroy(public_id=instance.image.public_id)
--- main_point/settings.py
+++ main_point/settings.py
@@ -20,22 +20,27 @@ import cloudinary.api
 import dj_database_url
 
 import environ
-env = environ.Env()
-environ.Env.read_env()
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+# environ.Env.read_env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('KEY')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-ALLOWED_HOSTS = ['mainpointelectronics.com', 'www.mainpointelectronics.com']
+DEBUG = True
+# ALLOWED_HOSTS = ['mainpointelectronics.com', 'www.mainpointelectronics.com']
+
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1', '0.0.0.0'])
 
 
 # Application definition
@@ -59,6 +64,7 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
