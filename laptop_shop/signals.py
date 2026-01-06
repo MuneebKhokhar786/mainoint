eff954373ad18c86cbd1c7f07e5352af98f7056c
M. Arhum Qayyum Khan <khanarhumqayyum@gmail.com>
2026-01-07 00:13:17 +0500

    feat(Delete) - Cloudinary auto Video deletion

--- laptop_shop/signals.py
+++ laptop_shop/signals.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.signals import user_logged_in
 from django.dispatch import receiver
-from django.db.models.signals import post_delete
-from .models import ProductImage
+from django.db.models.signals import pre_delete
+from .models import ProductImage, Collection, ProductVideo
 
 import cloudinary.uploader
 
@@ -9,8 +9,22 @@ import cloudinary.uploader
 def user_logged_in_handler(sender, request, user, **kwargs):
     request.session['user_id'] = user.id
 
-@receiver(post_delete, sender=ProductImage)
+@receiver(pre_delete, sender=Collection)
+@receiver(pre_delete, sender=ProductImage)
 def delete_cloudinary_image(sender, instance, **kwargs):
-    breakpoint()
+    print("PRE_DELETE:", sender.__name__)
+    print("PUBLIC ID:", getattr(instance.image, 'public_id', None))
     if instance.image:
-        cloudinary.uploader.destroy(public_id=instance.image.public_id)
+        result=cloudinary.uploader.destroy(public_id=instance.image.public_id, resource_type="image")
+        print("Cloudinary Image result: ", result)
+
+@receiver(pre_delete, sender=ProductVideo)
+def delete_cloudinary_video(sender, instance, **kwargs):
+    print("PRE_DELETE:", sender.__name__)
+    print("PUBLIC ID:", getattr(instance.video, 'public_id', None))
+    if instance.video:
+        try:
+            result=cloudinary.uploader.destroy(public_id=instance.video.public_id, resource_type="video")
+            print("Cloudinary Video Result: ", result)
+        except:
+            print("cant delete video")
