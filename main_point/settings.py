92b5a59e55aacef81a2267ddddca1b30c479ba59
M. Arhum Qayyum Khan <khanarhumqayyum@gmail.com>
2026-01-07 22:16:55 +0500

    Fix: update environment variable for secret key in settings.py

--- main_point/settings.py
+++ main_point/settings.py
@@ -26,7 +26,6 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
-# environ.Env.read_env()
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
@@ -34,7 +33,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
